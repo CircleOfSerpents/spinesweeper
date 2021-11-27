e6e2f0f5ed312f33b61b82b5b18cc0aeaf3785bb
CircleOfSerpents <93634752+CircleOfSerpents@users.noreply.github.com>
2021-11-27 00:03:04 -0800

    Fix static assets in temp build

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 # npm install
 # npm install -g typescript
 npm run build
-mv build/* src/server/public/*
+rm -r src/server/public
+mkdir src/server/public/
+mv build/* src/server/public/
 tsc -t es5 src/server/app.ts
-npm run start-server
